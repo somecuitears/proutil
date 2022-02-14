959304404a20ec4dc66fe56f974a64b55521676e
Sumeet Maharjan <smaharjan@qkly.io>
2022-02-14 22:25:02 +0545

    Updated scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullComponent } from './core/layout/full/full.component';
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		component: HomeComponent
+	},
+	{
+		path: 'util',
+		component: FullComponent,
+		canActivate: [],
+		// resolve:{data:}
+		children: [
+			{
+				path: '',
+				loadChildren: () => import('./modules/test/test-routing.module').then((m) => m.TestRoutingModule)
+			}
+		]
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/core/layout/layouts.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FullComponent } from './full/full.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    FullComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutsModule { }
